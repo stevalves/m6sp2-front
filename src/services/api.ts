629bdfdc1d2ef754f983860b45fe99a340d92906
Estevão A Santos <ealves1710@hotmail.com>
2023-08-22 06:36:52 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://m6sp2-back.onrender.com/",
+    baseURL: "https://ourc-api.onrender.com/",
     timeout: 50000
 })
 
 export const checkApi = axios.create({
-  baseURL: "https://m6sp2-back.onrender.com/",
+  baseURL: "https://ourc-api.onrender.com/",
   timeout: 500000
 })
